7435a256dafecc712a81fcb29c40e6f48053f8aa
jrlmx <bocaj.xml@gmail.com>
2024-01-24 23:34:57 -0500

    Add async/await to main function and improve error handling in crawl.js

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
+import { crawlPage } from './modules/crawl'
+
 /**
  * @return {void}
  */
-function main() {
-    //
+async function main() {
+    if (Bun.argv.length < 3) {
+        console.log('no url provided')
+        process.exit(1)
+    }
+
+    if (Bun.argv.length > 3) {
+        console.log('too many arguments provided')
+        process.exit(1)
+    }
+
+    const baseURL = Bun.argv[2]
+
+    console.log(`crawling ${baseURL}`)
+
+    const pages = await crawlPage(baseURL, baseURL, {})
+
+    for (let [page, hits] of Object.entries(pages)) {
+        console.log(`${page}, ${hits}`)
+    }
 }
 
 main()
--- modules/crawl.js
+++ modules/crawl.js
@@ -22,18 +22,18 @@ export async function getURLsFromHTML(html, baseURL) {
         .on('a', {
             element(el) {
                 const href = el.getAttribute('href')
-                if (
-                    href.startsWith('/') ||
-                    href.startsWith('http://') ||
-                    href.startsWith('https://')
-                ) {
+                if (href.startsWith('/')) {
                     try {
                         urls.push(new URL(href, baseURL).toString())
                     } catch (e) {
-                        console.log(`error with url ${href}: ${e.message}`)
+                        console.log(`error with relative url: ${e.message}`)
                     }
                 } else {
-                    console.log(`skipping invalid url: ${href}`)
+                    try {
+                        urls.push(new URL(href).toString())
+                    } catch (e) {
+                        console.log(`error with absolute url: ${e.message}`)
+                    }
                 }
             },
         })
@@ -42,4 +42,61 @@ export async function getURLsFromHTML(html, baseURL) {
         )
 
     return urls
+}
+
+/**
+ * @param {string} url
+ * @param {string} baseURL
+ * @returns {any}
+ */
+export async function crawlPage(baseURL, currentURL, pages) {
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+
+    if (pages[normalizedCurrentURL] > 0) {
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    pages[normalizedCurrentURL] = 1
+
+    console.log(`currently crawling ${currentURL}`)
+
+    try {
+        const response = await fetch(currentURL)
+
+        if (!response.ok) {
+            console.log(
+                `error fetching ${currentURL} with status: ${response.status}`,
+            )
+            return pages
+        }
+
+        const contentType = response.headers.get('content-type')
+
+        if (!contentType.includes('text/html')) {
+            console.log(
+                `non html response with ${currentURL} with content-type: ${contentType}`,
+            )
+            return pages
+        }
+
+        const html = await response.text()
+
+        const nextURLs = await getURLsFromHTML(html, baseURL)
+
+        for (let url of nextURLs) {
+            pages = await crawlPage(baseURL, url, pages)
+        }
+    } catch (e) {
+        console.log(`error fetching ${currentURL} with error: ${e.message}`)
+    }
+
+    return pages
 }
